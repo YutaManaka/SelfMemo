b62c627668fc5b306ece67599505c565bf80fcc3
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2021-12-28 22:01:59 +0900

    Initial Commit

---
+++ SelfMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SelfMemo
+//
+//  Created by 勇太_物販 on 2021/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
