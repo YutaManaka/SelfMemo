9effbdb11924cacafe3a4a5c54d586edb585f33d
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2021-12-30 12:22:43 +0900

    [add] 削除、移動機能

--- SelfMemo/ContentView.swift
+++ SelfMemo/ContentView.swift
@@ -28,13 +28,38 @@ struct ContentView: View {
         VStack {
             NavigationView {
                 List {
-                    ForEach( textLines ) { textLine in // appModel 内部で保持されている TextLine の全てを List の対象とする
-                        Text("\(textLine.id)  \(textLine.text)") // TextLine で保持している id と text を表示
+                    ForEach( textLines.freeze() ) { textLine in // appModel 内部で保持されている TextLine の全てを List の対象とする
+                        Text("\(textLine.text)") // TextLineで保持しているtextを表示
                     }
+                    .onDelete(perform: delete)
+                    .onMove(perform: move)
                 }
+                .navigationBarItems(
+                    leading: EditButton(),
+                    trailing: Button("Add", action: {
+                        let newName = "name" + String(Int.random(in: 0...2000))
+                        self.appModel.addTextLine(newName)
+                    })
+                )
                 .navigationBarTitle("セルフメモ") // タイトル
             }
         }
+    }
+    // 削除
+    func delete(at offsets:IndexSet) {
+        if let realm = textLines.realm {
+            try! realm.write {
+                realm.delete(textLines[offsets.first!])
+            }
+        } else {
+            textLines.remove(at: offsets.first!)
+        }
+    }
+    // 移動
+    func move(fromOffsets offsets: IndexSet, toOffset to: Int) {
+        textLines.realm?.beginWrite()
+        textLines.move(fromOffsets: offsets, toOffset: to)
+        try! textLines.realm?.commitWrite()
     }
 }
 
